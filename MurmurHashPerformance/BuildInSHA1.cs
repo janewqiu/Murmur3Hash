3cdb59530a53091c1244c7452710ce7e3964179f
arisoyang <ariso.yang@gmail.com>
2013-02-28 13:32:24 -0500

    draft version for murmur vs sha1 perf testing

---
+++ MurmurHashPerformance/BuildInSHA1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+namespace MurmurHashPerformance
+{
+    public class HashServices
+    {
+        public static string SHA1Hash(byte[] data)
+        {
+            string hashedValue = string.Empty;
+            try
+            {
+                HashAlgorithm hashClass = new SHA1CryptoServiceProvider();
+                byte[] hashedData = hashClass.ComputeHash(data);
+                return Convert.ToBase64String(hashedData);
+            }
+            catch
+            {
+            }
+            return hashedValue;
+        }
+
+
+        public static string Murmurhash(byte[] data)
+        {
+            string hashedValue = string.Empty;
+            try
+            {
+                Murmur3 hashClass = new Murmur3();
+                byte[] hashedData = hashClass.ComputeHash(data);
+                return Convert.ToBase64String(hashedData);
+            }
+            catch
+            {
+            }
+            return hashedValue;
+        }
+    }
+}
